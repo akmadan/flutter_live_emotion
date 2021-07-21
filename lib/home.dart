17fcf313bd1219a56b961ef582a30092c96e6aaa
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2021-07-21 14:11:45 +0530

    Camera Initialized

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+import 'main.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String result = '';
+
+  initCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {
+          cameraController!.startImageStream((imageStream) {
+            cameraImage = imageStream;
+            // runModel();
+          });
+        });
+      }
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model.tflite", labels: "assets/labels.txt");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
