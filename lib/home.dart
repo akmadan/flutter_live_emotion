9a53f07409034ed05fd5d5e0dff8fd8bd9727dca
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2021-07-21 14:17:29 +0530

    loadCamera() added

--- lib/home.dart
+++ lib/home.dart
@@ -25,7 +25,7 @@ class _HomeState extends State<Home> {
         setState(() {
           cameraController!.startImageStream((imageStream) {
             cameraImage = imageStream;
-            // runModel();
+            runModel();
           });
         });
       }
@@ -37,8 +37,58 @@ class _HomeState extends State<Home> {
         model: "assets/model.tflite", labels: "assets/labels.txt");
   }
 
+  runModel() async {
+    if (cameraImage != null) {
+      var recognitions = await Tflite.runModelOnFrame(
+          bytesList: cameraImage!.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
+          imageHeight: cameraImage!.height,
+          imageWidth: cameraImage!.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 2,
+          threshold: 0.1,
+          asynch: true);
+      recognitions!.forEach((element) {
+        setState(() {
+          result = element["label"];
+          print(result);
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Face Mask Detector"),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                height: MediaQuery.of(context).size.height - 170,
+                width: MediaQuery.of(context).size.width,
+                child: !cameraController!.value.isInitialized
+                    ? Container()
+                    : AspectRatio(
+                        aspectRatio: cameraController!.value.aspectRatio,
+                        child: CameraPreview(cameraController!),
+                      ),
+              ),
+            ),
+            Text(
+              result,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
